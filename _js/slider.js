6380498a43a0abbc84458051b22af18c87262a3b
Redvus <a.suvorof@gmail.com>
2023-01-16 09:53:30 +0400

    Общие доработки стиля

--- _js/slider.js
+++ _js/slider.js
@@ -11,7 +11,7 @@
         // sliderIntro.control('arrows' ,{insertTo:'#sliderIntroArrow', autohide:false});
         // sliderIntro.control('thumblist' ,{insertTo: '#sliderIntroThumbs', autohide:false, overVideo:true, dir:'h', speed:17, inset:false, arrows:false, hover:false, customClass:'', align:'bottom',type:'thumbs', margin:5, width:128, height:72, space:5, fillMode:'fill'});
 
-        if (document.body.clientWidth < 768) {
+        if (document.body.clientWidth < 820 || screen.width < 820) {
             sliderIntro.setup('sliderIntro', {
                 width: 420,
                 height: 740,
@@ -23,24 +23,28 @@
                 fullwidth: true,
                 preload: 0,
                 speed:20,
-                grabCursor: true,
-                swipe: true
+                grabCursor: false,
+                swipe: false,
+                mouse: false,
+                mobileBGVideo: true
                 // mouse: false
             });
-        } else if (document.body.clientWidth < 1280) {
+        } else if (document.body.clientWidth < 1280 || screen.width < 1280) {
             sliderIntro.setup('sliderIntro', {
                 width: 1280,
                 height: 720,
                 autoHeight: true,
                 space: 0,
                 loop: true,
-                view:'parallaxMask',
+                view:'basic',
                 layout:'fullscreen',
                 fullwidth: true,
                 preload: 0,
                 speed:20,
-                swipe: true,
-                mouse: true
+                grabCursor: false,
+                swipe: false,
+                mouse: false,
+                mobileBGVideo: true
             });
         } else {
             sliderIntro.setup('sliderIntro', {
@@ -49,16 +53,38 @@
                 autoHeight: true,
                 space: 0,
                 loop: true,
-                view:'parallaxMask',
+                view:'basic',
                 layout:'fullscreen',
                 fullwidth: true,
                 preload: 0,
                 speed:20,
-                grabCursor: true,
-                swipe: true
-        // 		mouse: false
+                grabCursor: false,
+                swipe: false,
+                mouse: false,
             });
         }
+    }
+
+    function sliderIntroMasterMobile() {
+        const sliderIntroMobile = new MasterSlider();
+        // sliderIntro.control('arrows' ,{insertTo:'#sliderIntroArrow', autohide:false});
+        // sliderIntro.control('thumblist' ,{insertTo: '#sliderIntroThumbs', autohide:false, overVideo:true, dir:'h', speed:17, inset:false, arrows:false, hover:false, customClass:'', align:'bottom',type:'thumbs', margin:5, width:128, height:72, space:5, fillMode:'fill'});
+
+        sliderIntroMobile.setup('sliderIntro', {
+            width: 420,
+            height: 740,
+            autoHeight: true,
+            space: 0,
+            loop: true,
+            view:'basic',
+            layout:'fullscreen',
+            fullwidth: true,
+            preload: 0,
+            speed:20,
+            grabCursor: true,
+            swipe: true
+            // mouse: false
+        });
     }
 
     function initPage() {
